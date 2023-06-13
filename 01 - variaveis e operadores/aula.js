d9d8062d65f3401aa7e51edfd9a19dd259b176a6
csargustavo <csar.gustavo.cg@gmail.com>
2023-06-13 20:50:25 -0300

    aula feita

--- 01 - variaveis e operadores/aula.js
+++ 01 - variaveis e operadores/aula.js
@@ -2,3 +2,20 @@
 let variavel = 10 / 10;
 
 console.log(variavel);
+
+// Faça um programa para calcular o valor de uma viagem.
+
+// Você terá 3 variáveis. Sendo elas:
+//  1 - Preço do combustível;
+//  2 - Gasto médio de combustível do carro por KM;
+//  3 - Distância em KM da viagem;
+
+// Imprima no console o valor que será gasto para realizar esta viagem.
+
+let comb = 5;
+let kmL = 12;
+let kmR = 100;
+
+console.log (kmR/kmL*comb)
+
+//esta pronto
